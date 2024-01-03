ffcc283792c417588467b643044d2a9f3501bd32
abdalahshaban <abdalahshaban@yahoo.com>
2024-01-04 01:23:44 +0200

    Add imports and functionality to AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Observable, interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +11,18 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-featurs';
+  title = 'angular-features';
+  private destroyRef = inject(DestroyRef)
+
+  onDestroy = this.destroyRef.onDestroy(() => {
+    console.log('AppComponent destroyed')
+    this.sub.unsubscribe()
+  })
+
+  counter$: Observable<number> = interval(1000)
+
+  sub = this.counter$.subscribe((val) => {
+    console.log(val)
+  })
+
 }
